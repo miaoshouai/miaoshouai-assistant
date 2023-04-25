85a11d006ffb1980e86f27155479003e77a218f3
Superag <superag@gmail.com>
2023-04-25 15:58:12 -0700

    handle case where json file is missing

--- configs/webui-user-launch.bat
+++ configs/webui-user-launch.bat
@@ -6,5 +6,5 @@ echo http://resource.miaoshouai.com 获取更多资源
 set GIT_PYTHON_REFRESH=quiet
 call update.bat
 
-python\python.exe webui.py  --api --deepdanbooru
+python\python.exe webui.py --autolaunch --api --xformers --medvram --deepdanbooru
 pause
