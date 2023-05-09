87d38da8be46e2d78acfba29f9ce7ac1d0162dbf
Superag <superag@gmail.com>
2023-05-09 16:20:51 -0700

    update assets from coding

--- scripts/runtime/msai_runtime.py
+++ scripts/runtime/msai_runtime.py
@@ -841,13 +841,18 @@ class MiaoshouRuntime(object):
         result = "Update successful, restart to take effective."
         try:
             print('Updating miaoshouai-assistant...')
-            repo = git.Repo(self.prelude.ext_folder)
+            repo = git.Repo(self.prelude.asset_folder)
             # Fix: `error: Your local changes to the following files would be overwritten by merge`,
             # because WSL2 Docker set 755 file permissions instead of 644, this results to the error.
             repo.git.fetch(all=True)
             repo.git.reset('origin', hard=True)
             if not dont_update_ms:
                 print('Updating model source...')
+                repo = git.Repo(self.prelude.asset_folder)
+                # Fix: `error: Your local changes to the following files would be overwritten by merge`,
+                # because WSL2 Docker set 755 file permissions instead of 644, this results to the error.
+                repo.git.fetch(all=True)
+                repo.git.reset('origin', hard=True)
                 self.install_preset_models_if_needed(True)
         except Exception as e:
             result = str(e)
